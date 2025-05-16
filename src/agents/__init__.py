93ccc979ac6b776ed9321446a538435bd57318dc
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2025-05-16 19:57:05 -0300

    export agents

---
+++ src/agents/__init__.py
@@ -0,0 +1,13 @@
+from ._cathie_wood import cathie_wood_agent
+from ._peter_lynch import peter_lynch_agent
+from ._ray_dalio import ray_dalio_agent
+from ._signal import SignalEvent
+from ._warren_buffett import warren_buffett_agent
+
+__all__ = [
+    "cathie_wood_agent",
+    "warren_buffett_agent",
+    "peter_lynch_agent",
+    "ray_dalio_agent",
+    "SignalEvent",
+]
