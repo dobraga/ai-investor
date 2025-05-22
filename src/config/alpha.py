2466dbcae50ce0bae860b86a33a6d268d6b9e213
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2025-05-22 07:35:32 -0300

    enhance alha config

--- src/config/alpha.py
+++ src/config/alpha.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass
 from pathlib import Path
 
 DAY_IN_SECONDS = 60 * 60 * 24
+CURRENT_DIR = Path(__file__).parent.parent.parent
 
 
 @dataclass
 class AlphaVantageConfig:
     api_key: str = "demo"
     timeout: int = 10  # timeout in seconds
-    cache_dir: Path = Path(".cache") / "alpha"  # cache directory
-    cache_timeout: int = 60 * DAY_IN_SECONDS  # cache timeout
+    cache_dir: Path = CURRENT_DIR / ".cache" / "alpha"  # cache directory
+    cache_timeout: int = 60 * DAY_IN_SECONDS  # cache timeout in seconds
